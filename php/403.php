72e8ecf488dfab008fe7ed12b3c750d2ccd2f442
pa_parappa <62480786+pa-parappa@users.noreply.github.com>
2021-09-20 22:55:39 -0500

    Update 403.php

--- php/403.php
+++ php/403.php
@@ -3,7 +3,7 @@ $title = '403 Forbidden';
 require_once('inc/header.php');
 ?>
 <div class="no-content">
-    <p>Sorry chum, you're not allowed to access this page.</p>
+    <p>Sorry, chum. You're not allowed to access this page.</p>
 </div>
 <?php
 require_once('inc/footer.php');
